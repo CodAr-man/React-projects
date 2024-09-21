fdfbf191e0003f6d1c9de939f79817a16e7ceaa2
CodAr-man <arman7info+github@gmail.com>
2024-09-21 12:22:23 +0530

    7th project

---
+++ miniContext/src/Context/UserContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const  UserContext = React.createContext();
+// React.Createcontext karke ek global oblect banaya fir isko export kiya 
+//usercontextprovider.jsx file mey jaha is object ki .provider value ko acess kiya ek usercontextprovider jsx fragment k andar
+export default UserContext;
